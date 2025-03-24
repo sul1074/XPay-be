b4a6c9b6e7bc424b00e13dabfed37b8c90eb3960
sul1074 <sul107485@gmail.com>
2025-03-24 21:06:09 +0900

    스프링 환경설정

---
+++ src/test/java/com/XPay/demo/XPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XPay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
